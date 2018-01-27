5d4271c6558e11a1251e04a403de97283f24b28c
Magicolo <straty91@hotmail.com>
2018-01-27 14:06:24 -0500

    poulah

---
+++ Assets/Scripts/ParticleManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ParticleManager : Singleton<ParticleManager>
+{
+	/// <summary>
+	/// Call moé Richard...
+	/// </summary>
+	public void GutExplosion(Vector3 position)
+	{
+
+	}
+}
